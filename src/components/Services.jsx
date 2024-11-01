4ea41180532d82d62af5fe26b75cb2e166ef0312
Avinash Mishra <137433922+kaushiki2104@users.noreply.github.com>
2024-11-01 21:58:19 +0530

    restor screen

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,9 +3,9 @@ import helpImage from '../assets/image/help.png';
 
 const Services=()=> {
   return (
-  <div className="container">
+  <div className="service-container">
     <div className="row">
-    <div className="col-3 text-container">
+    <div className="col-sm-3 text-container">
     <div class="card" style={{marginLeft:'10px'}}>
   <img class="card-img-top" src="helpImage" alt="Card image cap"/>
   <div class="card-body">
@@ -14,8 +14,8 @@ const Services=()=> {
   </div>
 </div> 
     </div>
-    <div className="col-3 text-container">
-    <div class="card"   >
+    <div className="col-sm-3 text-container">
+    <div class="card" style={{marginLeft:'10px'}}  >
   <img class="card-img-top" src="..." alt="Card image cap"/>
   <div class="card-body">
     <h5 class="card-title">Service 2 </h5>
@@ -23,8 +23,8 @@ const Services=()=> {
   </div>
 </div> 
     </div>
-    <div className="col-3 text-container">
-    <div class="card"  >
+    <div className="col-sm-3 text-container">
+    <div class="card"  style={{marginLeft:'10px'}} >
   <img class="card-img-top" src="..." alt="Card image cap"/>
   <div class="card-body">
     <h5 class="card-title">Service 3</h5>
@@ -32,8 +32,8 @@ const Services=()=> {
   </div>
 </div> 
     </div>
-    <div className="col-3 text-container">
-    <div class="card"   >
+    <div className="col-sm-3 text-container">
+    <div class="card"  style={{marginLeft:'10px'}} >
   <img class="card-img-top" src="..." alt="Card image cap"/>
   <div class="card-body">
     <h5 class="card-title">Service 4</h5>
