49686693d0f4d6d35e29e7aedf07973d224ae548
Avinash Mishra <137433922+kaushiki2104@users.noreply.github.com>
2024-11-02 21:15:14 +0530

    card designe

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,10 +3,10 @@ import helpImage from '../assets/image/help.png';
 
 const Services=()=> {
   return (
-  <div className="container">
+  <div className="container mt-3">
     <div className="row">
     <div className="col-3 text-container">
-    <div class="card" style={{marginLeft:'10px'}}>
+    <div class="card" >
   <img class="card-img-top" src="helpImage" alt="Card image cap"/>
   <div class="card-body">
     <h5 class="card-title">Service 1</h5>
@@ -37,6 +37,44 @@ const Services=()=> {
   <img class="card-img-top" src="..." alt="Card image cap"/>
   <div class="card-body">
     <h5 class="card-title">Service 4</h5>
+    
+  </div>
+    </div>
+    </div>
+    </div>
+    <div className="row mt-3">
+    <div className="col-3 text-container">
+    <div class="card" >
+  <img class="card-img-top" src="helpImage" alt="Card image cap"/>
+  <div class="card-body">
+    <h5 class="card-title">Service 5</h5>
+    
+  </div>
+</div> 
+    </div>
+    <div className="col-3 text-container">
+    <div class="card"   >
+  <img class="card-img-top" src="..." alt="Card image cap"/>
+  <div class="card-body">
+    <h5 class="card-title">Service 6 </h5>
+    
+  </div>
+</div> 
+    </div>
+    <div className="col-3 text-container">
+    <div class="card"  >
+  <img class="card-img-top" src="..." alt="Card image cap"/>
+  <div class="card-body">
+    <h5 class="card-title">Service 7</h5>
+   
+  </div>
+</div> 
+    </div>
+    <div className="col-3 text-container">
+    <div class="card"   >
+  <img class="card-img-top" src="..." alt="Card image cap"/>
+  <div class="card-body">
+    <h5 class="card-title">Service 8</h5>
     
   </div>
     </div>
